22a80370743be3da739c897839751ff35ec25128
Ofir Cohen <37571916+Play38@users.noreply.github.com>
2020-04-13 13:45:23 +0300

    Added curve

--- main.cpp
+++ main.cpp
@@ -14,15 +14,54 @@
 #define _LIB
 #define FREEGLUT_LIB_PRAGMAS 0*/
 
+
+class Point {
+public:
+	float x, y;
+	void setxy(float x2, float y2)
+	{
+		x = x2; y = y2;
+	}
+	//operator overloading for '=' sign
+	const Point& operator=(const Point& rPoint)
+	{
+		x = rPoint.x;
+		y = rPoint.y;
+		return *this;
+	}
+
+};
+
+int factorial(int n)
+{
+	if (n <= 1)
+		return(1);
+	else
+		n = n * factorial(n - 1);
+	return n;
+}
+
+float binomial_coff(float n, float k)
+{
+	float ans;
+	ans = factorial(n) / (factorial(k) * factorial(n - k));
+	return ans;
+}
+
 using namespace std;
 
 float red = 0.0, green = 0.0, blue = 0.0; // initiate it to black
 int tmpx, tmpy; // store the first point when shape is line, or circle
+int tmpcx1, tmpcy1, tmpcx2, tmpcy2, tmpcx3, tmpcy3, tmpcx4, tmpcy4;
 bool isSecond = false;
 float window_w = 500;
 float window_h = 500;
 
-int shape = 1; // 1:line, 2:circle
+Point abc[20];
+int curvePoints = 0;
+int clicks = 4;
+
+int shape = 1; // 1:line, 2:circle, 3:curve
 
 std::vector<Pixel> pixels;		// store all pixels
 
@@ -110,7 +149,6 @@ void myLine(int x1, int y1, int x2, int y2)
 	}
 }
 
-
 /**
  * Midpoint circle algorithm
  */
@@ -147,6 +185,27 @@ void myCircle(int x1, int y1, int x2, int y2)
 	}
 }
 
+//Calculate the bezier point
+Point drawBezier(Point PT[], double t) {
+	Point P;
+	P.x = pow((1 - t), 3) * PT[0].x + 3 * t * pow((1 - t), 2) * PT[1].x + 3 * (1 - t) * pow(t, 2) * PT[2].x + pow(t, 3) * PT[3].x;
+	P.y = pow((1 - t), 3) * PT[0].y + 3 * t * pow((1 - t), 2) * PT[1].y + 3 * (1 - t) * pow(t, 2) * PT[2].y + pow(t, 3) * PT[3].y;
+
+	return P;
+}
+
+Point drawBezierGeneralized(Point PT[], double t) {
+	Point P;
+	P.x = 0; P.y = 0;
+	for (int i = 0; i < clicks; i++)
+	{
+		P.x = P.x + binomial_coff((float)(clicks - 1), (float)i) * pow(t, (double)i) * pow((1 - t), (clicks - 1 - i)) * PT[i].x;
+		P.y = P.y + binomial_coff((float)(clicks - 1), (float)i) * pow(t, (double)i) * pow((1 - t), (clicks - 1 - i)) * PT[i].y;
+	}
+	//cout<<P.x<<endl<<P.y;
+	//cout<<endl<<endl;
+	return P;
+}
 
 void keyboard(unsigned char key, int xIn, int yIn)
 {
@@ -180,6 +239,24 @@ void mouse(int bin, int state, int x, int y)
 					else if (shape == 2)
 						myCircle(tmpx, tmpy, x, y);
 					isSecond = false;
+				}
+				if (shape == 3)
+				{
+					abc[curvePoints].setxy((float)x, (float)(y));
+					curvePoints++;
+
+					if (curvePoints == clicks)
+					{
+						Point p1 = abc[0];
+						/* Draw each segment of the curve.Make t increment in smaller amounts for a more detailed curve.*/
+						for (double t = 0.0; t <= 1.0; t += 0.005)
+						{
+							Point p2 = drawBezierGeneralized(abc, t);
+							myLine(p1.x, p1.y, p2.x, p2.y);
+							p1 = p2;
+						}
+						curvePoints = 0;
+					}
 				}
 			}
 		}
@@ -235,6 +312,7 @@ void createOurMenu()
 	int shapeMenu = glutCreateMenu(processShapeMenu);
 	glutAddMenuEntry("Line", 1);
 	glutAddMenuEntry("Circle", 2);
+	glutAddMenuEntry("Curve", 3);
 
 
 
